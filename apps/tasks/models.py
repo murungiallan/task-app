c4f1c7f75f75df0e0a2d9c24b69d5e971df80350
Allan <allancheboiw0@gmail.com>
2025-03-08 12:24:00 +0800

    refactoring folder

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -6,5 +6,8 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-    created_At = models.DateField()
-    updated_At = models.DateField()
+    created_At = models.DateField(auto_now_add=True)
+    updated_At = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
